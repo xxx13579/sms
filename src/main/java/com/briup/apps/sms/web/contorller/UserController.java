a7e17656287ef9f02ae610c95a86607332a5e91f
许效贤 <1084152522@qq.com>
2019-07-09 10:58:07 +0800

    [许效贤]为用户控制系统添加了注释

--- src/main/java/com/briup/apps/sms/web/contorller/UserController.java
+++ src/main/java/com/briup/apps/sms/web/contorller/UserController.java
@@ -3,7 +3,7 @@ package com.briup.apps.sms.web.contorller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//xxxx
 @RestController
 @RequestMapping("/user")
 public class UserController {
