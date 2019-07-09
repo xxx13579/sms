bef5e1c5c66ef56728ed1de26da31154e4e26220
许效贤 <1084152522@qq.com>
2019-07-09 10:54:06 +0800

    [许效贤]项目初始化提交

---
+++ src/main/java/com/briup/apps/sms/web/contorller/UserController.java
@@ -0,0 +1,14 @@
+package com.briup.apps.sms.web.contorller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@GetMapping("findAll")
+	public String findAll() {
+		return "hello springboot";
+	}
+}
