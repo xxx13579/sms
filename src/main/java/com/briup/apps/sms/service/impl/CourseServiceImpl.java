3b8839c51cd0303813065ff728abf96dc4e28dd5
王健 <1822341646@qq.com>
2019-07-10 12:14:39 +0800

    [王健]创建完成关于Course查询操作

---
+++ src/main/java/com/briup/apps/sms/service/impl/CourseServiceImpl.java
@@ -0,0 +1,44 @@
+package com.briup.apps.sms.service.impl;
+import java.util.List;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.sms.bean.Course;
+import com.briup.apps.sms.bean.School;
+import com.briup.apps.sms.dao.CourseDao;
+import com.briup.apps.sms.dao.SchoolDao;
+import com.briup.apps.sms.service.CourseService;
+import com.briup.apps.sms.service.SchoolService;
+
+/**
+ * 校园逻辑逻辑处理的实现类
+ * */
+@Service
+public abstract class CourseServiceImpl 
+	implements CourseService {
+	// 依赖注入，实例化CourseDao并且赋值给schoolDao这个变量
+	@Resource
+	private CourseDao courseDao;
+	@Override
+	public List<Course> selectAll() {
+		// TODO Auto-generated method stub
+		return courseDao.selectAll();
+	}
+
+	@Override
+	public void saveOrUpdate(Course course) throws Exception {
+		if(course.getId()==null) {
+			courseDao.insert(course);
+		} 
+		else {
+			courseDao.update(course);
+	}
+
+
+	
+	}
+
+}
+
+
--- src/main/java/com/briup/apps/sms/web/contorller/CourseController.java
+++ src/main/java/com/briup/apps/sms/web/contorller/CourseController.java
@@ -1,5 +1,26 @@
 package com.briup.apps.sms.web.contorller;
 
-public class CourseController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.briup.apps.sms.bean.Course;
+import com.briup.apps.sms.service.CourseService;
+
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+	
+	@Autowired
+	
+	private CourseService courseService;
+
+	// http://localhost:8080/Course/selectAll
+	@GetMapping("selectAll")
+	public List<Course> selectAll(){
+		return courseService.selectAll();
+	}
 }
