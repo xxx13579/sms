36f40d76e84822028ab03f02e13010672bac2ef7
Administrator <>
2019-07-10 14:23:31 +0800

    [魏丽红]完成校园信息的查询

--- src/main/java/com/briup/apps/sms/service/ClazzService.java
+++ src/main/java/com/briup/apps/sms/service/ClazzService.java
@@ -1,5 +1,12 @@
 package com.briup.apps.sms.service;
 
+import java.util.List;
+
+import com.briup.apps.sms.bean.School;
+
 public interface ClazzService {
 
+	List<School> selectAll();
+
+	void saveOrUpdate(School school) throws Exception;
 }
