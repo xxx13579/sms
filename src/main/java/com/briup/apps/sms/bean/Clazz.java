c8800a3319f86abae8fb8929ae218a3c722dac91
Administrator <>
2019-07-10 10:23:14 +0800

    [魏丽红]三层架构搭建，校园信息管理部分代码提交

---
+++ src/main/java/com/briup/apps/sms/bean/Clazz.java
@@ -0,0 +1,26 @@
+package com.briup.apps.sms.bean;
+
+
+public class Clazz {
+	private long id;
+	private String name;
+	private String begin_time;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBegin_time() {
+		return begin_time;
+	}
+	public void setBegin_time(String begin_time) {
+		this.begin_time = begin_time;
+	}
+}
