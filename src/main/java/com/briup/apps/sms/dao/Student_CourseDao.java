0de63df4ed858e1acb9e67885e72d31bfdad7152
wjj <you@example.com>
2019-07-11 10:43:06 +0800

    [王军军]上传

--- src/main/java/com/briup/apps/sms/dao/Student_CourseDao.java
+++ src/main/java/com/briup/apps/sms/dao/Student_CourseDao.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.briup.apps.sms.bean.Student_Course;
 
 public interface Student_CourseDao {
-	//查询所有
-	List<Student_Course>selectAll();
-	//插入
-void insert(Student_Course  student_course );
-	//更新
+	   //查询所有
+	 List<Student_Course>selectAll();
+	   //插入
+     void insert(Student_Course  student_course );
+	  //更新
 	void update(Student_Course student_course);
-}
+      //通过ID删除
+   void deleteById(long id);
+ }
