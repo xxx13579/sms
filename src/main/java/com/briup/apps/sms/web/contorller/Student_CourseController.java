f83c25e95134d0d09dedf3aca44ba6346d2eac25
wjj <1836941845@qq.com>
2019-07-11 14:58:39 +0800

    [王军军]修改

--- src/main/java/com/briup/apps/sms/web/contorller/Student_CourseController.java
+++ src/main/java/com/briup/apps/sms/web/contorller/Student_CourseController.java
@@ -1,11 +1,10 @@
 package com.briup.apps.sms.web.contorller;
-
-
-
+	
 import java.util.List;
-
+	
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,25 +16,38 @@ import com.briup.apps.sms.service.Student_CourseService;
 public class Student_CourseController{
 
 	@Autowired
-	private Student_CourseService student_courseService;
-
-	// http://localhost:8080/student_courseService;
+	private Student_CourseService  student_courseService;
+	@PostMapping("saveOrUpdate")
+	public String saveOrUpdate(Student_Course student_course) {
+		try {
+			student_courseService.saveOrUpdate(student_course);
+			return "保存或更新成功";
+		}catch(Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+	}
+	// http://localhost:8080/student_course/selectAll
 	@GetMapping("selectAll")
 	public List<Student_Course> selectAll(){
 		return student_courseService.selectAll();
 	}
-
-//http://localhost:8080/student_course/deleteById?id=3
-		@GetMapping("deleteById")
-		public String deleteById(long id) {
-			try {
-				student_courseService.deleteById(id);
-				return "删除成功";
-			} catch (Exception e) {
-				// 打印异常信息，返回异常信息
-				e.printStackTrace();
-				return e.getMessage();
-			}
+	@GetMapping("deleteById")
+	public String deleteById(long id) {
+		try {
+			student_courseService.deleteById(id);
+			return "删除成功";
+		} catch (Exception e) {
+			// 打印异常信息，返回异常信息
+			e.printStackTrace();
+			return e.getMessage();
 		}
+	}
+
+
+
+
 }
+		
+
 	
